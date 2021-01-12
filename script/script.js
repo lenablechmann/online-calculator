3a4f278a456cfb1c172b66f2805622eec8c43a71
Lena Blechmann <lenablechmann@gmail.com>
2021-01-12 14:37:36 +0100

    trim incoming user input to defined length!

--- script/script.js
+++ script/script.js
@@ -1,11 +1,6 @@
+// making sure the DOM is loaded before the script is run
+// using this instead of end of <body> for a more streamlined html
 document.addEventListener("DOMContentLoaded", function () {
-  // making sure the DOM is loaded before the script is run
-  // using this instead of end of <body> for a more streamlined html
-  let displayedText = [];
-  let userNumber = "";
-  // arrays for reverse polish notation (unshift() will have to be used)
-  let numberStack = [];
-  let operatorStack = [];
 
   // variables for DOM modification
   const digitsInput = document.querySelectorAll(".digit");
@@ -13,31 +8,43 @@ document.addEventListener("DOMContentLoaded", function () {
   const display = document.querySelector(".user-input");
   const result = document.querySelector(".result");
 
-  // add a clear button functionality
+  // variables for user input
+  let displayedText = [];
+  let userNumber = "";
+  let numberStack = [];
+  let operatorStack = [];
+  const maxLength = 35;
+
+  // AC - clear functionality
   document.getElementById("clear").onclick = function clearDisplay() {
     //clearing out all of the memory (hopefully, there aint no pointers here)
-    displayedText = null, userNumber = null, numberStack = null, operatorStack = null;
+    displayedText = null;
+    userNumber = null;
+    numberStack = null;
+    operatorStack = null;
     // setting arrays, so that .length can work everywhere
     displayedText = [];
     userNumber = "";
     numberStack = [];
     operatorStack = [];
     // resetting display as well
-    display.textContent = displayedText;
-    result.textContent = displayedText;
+    display.textContent = "";
+    result.textContent = "";
   };
+
   // append new user input digit to displayed string if number is plausible
   digitsInput.forEach((btn) => {
     btn.addEventListener("click", function (event) {
       const newDigit = this.id;
       if (checkNumber(userNumber + newDigit)) {
         userNumber += newDigit;
-        // if((displayedText.join("") + userNumber).length > )
-        display.textContent = displayedText.join("") + userNumber;
+        let allUserInput = displayedText.join("") + userNumber;
+        display.textContent = allUserInput.slice(-maxLength);
       }
     });
   });
 
+  //what happens if you press an operator button
   operatorInput.forEach((btn) => {
     btn.addEventListener("click", function (event) {
       // avoids operator spam
@@ -51,19 +58,16 @@ document.addEventListener("DOMContentLoaded", function () {
         displayedText.push(userNumber);
         displayedText.push(this.id);
         userNumber = "";
-        display.textContent = displayedText.join("");
+        display.textContent = displayedText.join("").slice(-maxLength);
       }
     });
   });
-
 });
 
 // various functions
 
-
 function checkNumber(numString) {
   // checks for duplicate . and 0 at start using regex
   // source: https://stackoverflow.com/questions/16779871/javascript-function-to-avoid-two-decimal-points-ex-12-1-1-in-html-input-text
-  // I'm no RegEx wizard (yet?)
   return /^[+-]?(([1-9][0-9]*)?[0-9](\.[0-9]*)?|\.[0-9]+)$/.test(numString);
 }
